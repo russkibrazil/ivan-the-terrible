309f1a373623b40ad0c84a805afeeee761c6ac44
Igor Pereira <igorp9971@gmail.com>
2021-05-13 11:02:31 -0300

    Instalações adicionais

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+import './styles/app.css';
+// start the Stimulus application
+import './bootstrap';
+import './styles/app.css';
+import $ from 'jquery';
+require('bootstrap');
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'remixicon/fonts/remixicon.css';
