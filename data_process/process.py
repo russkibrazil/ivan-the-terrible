2d12f0724df999b8c169f5fa64f4198e80201518
Igor Pereira <igorp9971@gmail.com>
2021-06-23 22:50:00 -0300

    AMEC para seio e mamadeira

--- data_process/process.py
+++ data_process/process.py
@@ -1,20 +1,21 @@
 from datetime import date, datetime
 import pandas
 import numpy
+import matplotlib.pyplot as mpl
 import mysql.connector
 
 
-def estimador_amamentacao_AMEX(idade, inicio, fim) -> float:
-    x = date.today() - date.fromisoformat(idade)
-    z = datetime.fromisoformat(fim) - datetime.fromisoformat(inicio)
+def estimador_amamentacao_AMEX(dhInicio, dhFim, idade, dh) -> float:
+    x = dh - date.fromisoformat(idade)
+    z = datetime.fromisoformat(dhFim) - datetime.fromisoformat(dhInicio)
     return 591 - 0.7*x.days + 0.76*z.seconds*60
 
-def estimador_amamentacao_AMEC(idade, kcalorias) -> float:
-    x = date.today() - date.fromisoformat(idade)
+def estimador_amamentacao_AMEC(kcalorias, idade, dh) -> float:
+    x = dh - date.fromisoformat(idade)
     return 755-0.48*x.days-0.59*kcalorias
 
-def estimador_amamentacao_nMamadas(idade, nMamadas) -> float:
-    x = date.today() - date.fromisoformat(idade)
+def estimador_amamentacao_nMamadas(nMamadas, idade, dh) -> float:
+    x = dh - date.fromisoformat(idade)
     return 489-0.63*x.days+13.45*nMamadas
 
 def agrupamento_mamadas_proximas():
@@ -58,7 +59,7 @@ nSolido = cursor.rowcount
 dfP = pandas.DataFrame(seio, columns=columnSeio)
 dfM = pandas.DataFrame(mamadeira, columns=columnMamadeira)
 dfS = pandas.DataFrame(solido, columns=columnSolido)
-resLeite = None
+resLeite = pandas.DataFrame()
 
 if nSeio > 0:
     amex = True
@@ -71,41 +72,112 @@ if nSeio > 0:
             amex = False
     if amex:
         estima_volume_amex = dfM.apply('estimador_amamentacao_AMEX', 1, False, 'reduce')
+        #calcular a média de mamadas por dia, para então...
+        agrupa_ame = estima_volume_amex.groupby('data').count()
+        media_diaria_esperada_ame = round(agrupa_ame['dh'].apply(numpy.average))
+
+        #interpolar os resultados presentes para deduzir os pontos de mamada ausentes. https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.interpolate.html
+        #listar as deduções em tabela
     else:
         #AMEC estimator?
         pass
-    leiteMaterno_mesclado = pandas.concat([resLeite, dfM], ignore_index=True)
+    leiteMaterno_mesclado = pandas.concat([resLeite, dfP], ignore_index=True)
     #agrupar leiteMaterno_mesclado por data
     horarios_leitem = leiteMaterno_mesclado['dhInicio']
     leiteMaterno_mesclado['dh'].fillna(horarios_leitem, inplace=True)
-    leiteMaterno_mesclado['data'] = leiteMaterno_mesclado['dh'].date
-    leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['dh'].time
+    leiteMaterno_mesclado['data'] = leiteMaterno_mesclado['dh'].dt.date
+    leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['dh'].dt.time
 
     #para cada DF do agrupamento, montar uma list contendo a quantidade de mamadas encontradas
-    qtd_mamadas = leiteMaterno_mesclado.groupby('data').count().apply(numpy.average,0) #seria melhor enviar a quantidade exata das mamadas do dia para o estimador, mas vamos simplificar por hora com a média
+    qtd_mamadas = leiteMaterno_mesclado.groupby('data').count()
 
-    estima_volume_nMamadas = leiteMaterno_mesclado.apply('estimador_amamentacao_nMamadas', 1, False, 'reduce', {'nMamadas': qtd_mamadas}) #mesclar os resultados quando se trata de mamadas em mamadeira antes de aplicar a função
+    resLeite['data'] = resLeite['dh'].dt.date
+    volume_mamadeira_somado = resLeite.groupby('data')['volume'].apply(numpy.sum)
+    estimativa_diaria = []
+    for index, valor in qtd_mamadas['dhInicio'].iteritems():
+        estimativa_diaria.append(estimador_amamentacao_nMamadas(valor, idadec, index))
 
-    #comparar resultados e juntar os melhores ao dfM ==> no caso, vou partir para a média novamente
-    resultados_estimadores = pandas.DataFrame([estima_volume_amex, estima_volume_nMamadas])
-    resultados_estimadores['data'] = resultados_estimadores['dh'].date
-    resultados_estimadores['hora'] = resultados_estimadores['dh'].time
-    resultados_estimadores.drop('dh',1)
-    resultados_estimadores.groupby('data').agg({'volume': numpy.average})
+    estimativa_diaria = pandas.Series(estimativa_diaria, qtd_mamadas.index)
 
-    #interpolar os resultados presentes para deduzir os pontos de mamada ausentes. Utilizar métodos lineares para a dedução
+    if amex and nMamadeira > 0:
+        resultados_estimadores = pandas.DataFrame([estima_volume_amex, estima_volume_nMamadas])
+        resultados_estimadores['data'] = resultados_estimadores['dh'].dt.date
+        resultados_estimadores['hora'] = resultados_estimadores['dh'].dt.time
+        resultados_estimadores.drop('dh',1)
+        resultados_estimadores.groupby('data').agg({'volume': numpy.average})
+        resultados_estimadores.plot(x='data', y='volume', kind='bar', use_index=False)
+    else:
+        if amex==False:
+            estimativa_diaria.plot(kind='bar')
+        else:
+            pass
+    media_por_mamada_seio = ((estimativa_diaria-volume_mamadeira_somado) / qtd_mamadas['dh']).repeat(qtd_mamadas['dhInicio'].array) #comparar em gráfico de barras empilhadas os dois tipos de leite, invés da barra somatório
+    media_por_mamada_seio.rename('idx', inplace=True)
+    media_por_mamada_seio.index = leiteMaterno_mesclado[leiteMaterno_mesclado['volume'].isna()].index
     #plotar: volumes (barras) e scatter (horarios); as mamadeiras em legenda especial no caso do scatter
-    leiteMaterno_mesclado.plot(x='dh', y='volume', kind='scatter', use_index=False)
-    resultados_estimadores.plot(x='data', y='volume', kind='bar', use_index=False)
-    #invocar o matplotlib para salvar as imagens
+    leiteMaterno_mesclado['volume'].fillna(media_por_mamada_seio, inplace=True)
+    leiteMaterno_mesclado['hora'] = pandas.Series(leiteMaterno_mesclado['dh'].dt.time, dtype='string') #codificar como número?
+    leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['hora'].str.replace(':','', regex=False)
+    leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['hora'].str.slice(stop=4)
+    leiteMaterno_mesclado['hora'] = pandas.Series(leiteMaterno_mesclado['hora'], dtype='int')
+    leiteMaterno_mesclado.plot.scatter(x='hora', y='volume', use_index=False)
+     #invocar o matplotlib para salvar as imagens
 
 if nMamadeira > 0:
     #retirar resultados contendo leite materno
-    if resLeite != None:
-        dfM.drop(dfM['alimento'] == 'LEITEM', inplace=True)
+    if resLeite.empty != False:
+        mamadeira = dfM[dfM['alimento'] != 'LEITEM']
+    else:
+        mamadeira = dfM
     #seaprar os tipos de alimentos disponíveis em df ou grupos (o que por sua vez já permite pegar os dataframes separados)
     #criar tabelas-somatório com as médias diárias de consumo de cada um dos tipos de alimentos encontrados
-    dfM.groupby(['dh','alimento']).agg({'volume': numpy.average})
+    mamadeira['data'] = pandas.Series(dfM['dh'].dt.date)
+    mamadeira.drop('dh', 1)
+    resultados_mamadeira = mamadeira.groupby(['data','alimento']).agg({'volume': numpy.sum})
+
+    idx = resultados_mamadeira.index.to_frame(index=False)
+    idx.drop('alimento', 1, inplace=True)
+    idx.drop_duplicates(inplace=True)
+    ticks = pandas.Series(idx['data'], dtype='datetime64[ns]')
+    idx['data'] = ticks
+    idx['dia'] = idx['data'].dt.day
+    idx['mes'] = idx['data'].dt.month
+    idx['ano'] = idx['data'].dt.year
+    idx['ano'] = idx['ano'].apply(lambda x: x* (3600*24*30*12))
+    idx['mes'] = idx['mes'].apply(lambda x: x* (3600*24*30))
+    idx['dia'] = idx['dia'].apply(lambda x: x* (3600*24))
+    idx.drop('data',1, inplace=True)
+    novo_index_data = idx.apply(numpy.sum,1, result_type='reduce').to_list()
+    resultados_mamadeira.index = resultados_mamadeira.index.set_levels(novo_index_data, 0)
+    resultados_mamadeira.reset_index(inplace=True)
+
+    diferentes_alimentos = resultados_mamadeira['alimento'].unique()
+    diferentes_datas = resultados_mamadeira['data'].unique()
+
+    y_offset = numpy.zeros(len(resultados_mamadeira['data'].unique()))
+    fig, ax = mpl.subplots()
+
+    for tipo in diferentes_alimentos:
+        dados = resultados_mamadeira[resultados_mamadeira['alimento'] == tipo]
+
+        if len(dados['data']) != len(diferentes_datas):
+            ausentes = diferentes_datas[numpy.in1d(diferentes_datas, dados['data'].array, invert=True)]
+            arr_dados_ausentes = numpy.zeros((len(ausentes),3))
+            for i in range(len(ausentes)):
+                arr_dados_ausentes[i][0] = ausentes[i]
+                arr_dados_ausentes[i][1] = 0
+                arr_dados_ausentes[i][2] = 0
+            dados = dados.append(pandas.DataFrame(arr_dados_ausentes, columns=dados.columns), ignore_index=True)
+            dados = dados.sort_values(by=['data'])
+        dados['data'] = pandas.Series([x for x in range(len(diferentes_datas))])
+        ax.bar(dados['data'], dados['volume'], width=0.5, bottom=y_offset, label=tipo)
+        y_offset = y_offset + dados['volume']
+
+    ax.legend()
+    ax.set_ylabel('Volume, ml')
+    ax.set_xlabel('Data')
+    mpl.xticks(numpy.arange(0,len(diferentes_datas)), ticks.dt.date, rotation=90)
+    mpl.draw()
     #imprimir as tabelas
 
 if nSolido > 0:
