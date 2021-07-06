83821cb9473c4d86381c00bf85afe2870fee0e57
Igor Pereira <igorp9971@gmail.com>
2021-07-06 16:18:58 -0300

    Correções do processamento do AME

--- data_process/process.py
+++ data_process/process.py
@@ -6,9 +6,9 @@ import mysql.connector
 import nltk
 import argparse
 
-def estimador_amamentacao_AMEX(dhInicio, dhFim, idade, dh) -> float:
-    x = dh - date.fromisoformat(idade)
-    z = datetime.fromisoformat(dhFim) - datetime.fromisoformat(dhInicio)
+def estimador_amamentacao_AMEX(dhInicio, dhFim, idade) -> float:
+    x = dhInicio.date() - date.fromisoformat(idade)
+    z = dhFim - dhInicio
     return 591 - 0.7*x.days + 0.76*z.seconds*60
 
 def estimador_amamentacao_AMEC(kcalorias, idade, dh) -> float:
@@ -18,15 +18,6 @@ def estimador_amamentacao_AMEC(kcalorias, idade, dh) -> float:
 def estimador_amamentacao_nMamadas(nMamadas, idade, dh) -> float:
     x = dh - date.fromisoformat(idade)
     return 489-0.63*x.days+13.45*nMamadas
-
-def agrupamento_mamadas_proximas():
-    pass
-
-def processar_mamadeira():
-    pass
-
-def processar_alimentcao_solida():
-    pass
 
 def switch_tipo_liquido(tipo : str) -> str:
     switcher = {
@@ -95,6 +86,8 @@ idadec = crianca[2]
 dfP = pandas.DataFrame(seio, columns=columnSeio)
 dfM = pandas.DataFrame(mamadeira, columns=columnMamadeira)
 dfS = pandas.DataFrame(solido, columns=columnSolido)
+dfP['dhInicio'] = pandas.to_datetime(dfP['dhInicio'], format='%d-%m-%Y %H:%M:%S')
+dfP['dhFim'] = pandas.to_datetime(dfP['dhFim'], format='%d-%m-%Y %H:%M:%S')
 resLeite = pandas.DataFrame()
 
 if nSeio > 0:
@@ -108,64 +101,203 @@ if nSeio > 0:
             amex = False
 
     if amex:
-        estima_volume_amex = dfM.apply('estimador_amamentacao_AMEX', 1, False, 'reduce')
-        #calcular a média de mamadas por dia, para então...
-        agrupa_ame = estima_volume_amex.groupby('data').count()
-        media_diaria_esperada_ame = round(agrupa_ame['dh'].apply(numpy.average))
+        #PREPARAÇÃO
+        estimativa_diaria = dfP.copy()
+        estimativa_diaria.loc[:,'volume'] = 0
+        estimativa_diaria.loc[:, 'duracao'] = 0
+        estimativa_diaria['dInicio'] = estimativa_diaria['dhInicio'].dt.date
+        estimativa_diaria['hInicio'] = estimativa_diaria['dhInicio'].dt.time
+        estimativa_diaria['dFim'] = estimativa_diaria['dhFim'].dt.date
+        estimativa_diaria['hFim'] = estimativa_diaria['dhFim'].dt.time
 
-        #interpolar os resultados presentes para deduzir os pontos de mamada ausentes. https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.interpolate.html
-        #listar as deduções em tabela
+        #ESTIMATIVA ATRAVÉS DE REGRESSORES
+        agrupa_ame = estimativa_diaria.groupby('dInicio')
+        agrupa_ame_conta = agrupa_ame.count()
+
+        volumesX = []
+        volumesN = []
+        if resLeite.empty:
+            for index, valores in estimativa_diaria.iterrows():
+                volumesX.append(estimador_amamentacao_AMEX(valores['dhInicio'], valores['dhFim'], idadec))
+                volumesN.append(estimador_amamentacao_nMamadas(agrupa_ame_conta.loc[valores['dInicio'], 'dhInicio'], idadec, valores['dInicio']))
+
+            volumes = numpy.array([volumesX, volumesN])
+            volumes = numpy.mean(volumes, 0)
+            estimativa_diaria['volume'] = pandas.Series(volumes)
+
+            #vamos colcoar os volumes estimados para cada sessão
+            agrupa_ame_tempo = estimativa_diaria.apply(lambda x: x['dhFim'] - x['dhInicio'], axis=1)
+            agrupa_ame_tempo = agrupa_ame_tempo.apply(lambda x: x.seconds / 60)
+            estimativa_diaria['duracao'] = agrupa_ame_tempo
+
+            agrupa_ame = estimativa_diaria.groupby('dInicio')
+            agrupa_ame_soma_volume = agrupa_ame['volume'].agg('mean')
+            agrupa_ame_soma_tempo = agrupa_ame['duracao'].agg('sum')
+
+            estimativa_diaria['volume'] = estimativa_diaria.apply(lambda x: agrupa_ame_soma_volume.loc[x['dInicio']] / agrupa_ame_soma_tempo.loc[x['dInicio']] * agrupa_ame_tempo[int(x.name)], axis=1)
+
+        else:
+            leiteMaterno_mesclado = pandas.concat([resLeite, dfP], ignore_index=True)
+
+            #se houver registros de LM em mamadeira, organziar as colunas de data e hora, além de obter a quantidade de leite ingerido por mamadeira
+            horarios_leitem = leiteMaterno_mesclado['dhInicio']
+            volume_mamadeira_somado = pandas.Series()
+            leiteMaterno_mesclado['dh'].fillna(horarios_leitem, inplace=True)
+            leiteMaterno_mesclado['dh'] = pandas.to_datetime(leiteMaterno_mesclado['dh'])
+            leiteMaterno_mesclado['dInicio'] = leiteMaterno_mesclado['dh'].dt.date
+            leiteMaterno_mesclado['hInicio'] = leiteMaterno_mesclado['dh'].dt.time
+            resLeite['data'] = resLeite['dh'].dt.date
+            volume_mamadeira_somado = resLeite.groupby('data')['volume'].apply(numpy.sum)
+
+            #para cada DF do agrupamento, montar uma list contendo a quantidade de mamadas encontradas
+            qtd_mamadas = leiteMaterno_mesclado.groupby('dInicio').count()
+
+            #estimando o volume de cada mamada no seio materno
+            resultados_estimador = []
+            for index, valor in qtd_mamadas['dh'].iteritems():
+                resultados_estimador.append(estimador_amamentacao_nMamadas(valor, idadec, index))
+
+            #preparo e inserção dos valores de mamada de leite
+            _ = pandas.Series(resultados_estimador, qtd_mamadas.index)
+            _ = _.repeat(qtd_mamadas['dhFim'].to_list())
+            _ = _ / qtd_mamadas['dhFim'].repeat(qtd_mamadas['dhFim'].to_list())
+            _ = _.reset_index()
+            _.index = leiteMaterno_mesclado[leiteMaterno_mesclado['volume'].isna()].index
+            _.rename(columns={0: 'volume'}, inplace=True)
+
+            leiteMaterno_mesclado['volume'].update(_['volume'])
+            estimativa_diaria = leiteMaterno_mesclado.copy()
+
+            # calculo de marcações temporais
+            agrupa_ame_tempo = estimativa_diaria.apply(lambda x: x['dhFim'] - x['dhInicio'], axis=1)
+            agrupa_ame_tempo = agrupa_ame_tempo.apply(lambda x: x.seconds / 60)
+            estimativa_diaria['duracao'] = agrupa_ame_tempo
+            estimativa_diaria['dFim'] = estimativa_diaria['dhFim'].dt.date
+            estimativa_diaria['hFim'] = estimativa_diaria['dhFim'].dt.time
+            estimativa_diaria.drop('dhInicio', axis=1, inplace=True)
+            estimativa_diaria.rename({'dh':'dhInicio'}, axis=1, inplace=True)
+
+        #agrupamento de mamadas
+        passo = 1
+        drops = []
+        to_update = pandas.DataFrame()
+        for i in range(len(estimativa_diaria)-1):
+            row = estimativa_diaria.iloc[i]
+            if row['lado'] == numpy.NaN:
+                continue
+
+            proximo = estimativa_diaria.iloc[i+passo]
+            while (passo > 0):
+                if (proximo['dhInicio'] - row['dhFim']).seconds < 300:
+                    row['volume'] = row['volume'] + proximo['volume']
+                    row['dhFim'] = proximo['dhFim']
+                    row['dFim'] = proximo['dFim']
+                    row['hFim'] = proximo['hFim']
+                    row['duracao'] = row['duracao'] + proximo['duracao']
+                    drops.append(i+passo)
+                    passo = passo + 1
+                    try:
+                        proximo = estimativa_diaria.iloc[i+passo]
+                    except:
+                        i = i + passo
+                        passo = 0
+                else:
+                    i = i + passo
+                    passo = 0
+            passo = 1
+            to_update = to_update.append(row)
+
+        #
+        estimativa_diaria.update(to_update)
+        estimativa_diaria.drop(index=drops, inplace=True)
+        # #estimativa_diaria.sort_values(by='hInicio', inplace=True)
+
+        # #calcular a média de mamadas por dia, para então...
+        agrupa_ame = estimativa_diaria.groupby('dInicio')
+        # agrupa_ame_conta = agrupa_ame.count()
+        # agrupa_ame_soma = agrupa_ame.agg({'dhInicio': 'count', 'volume': 'mean'})
+        # agrupa_ame_soma = agrupa_ame_soma['volume'] / agrupa_ame_soma['dhInicio']
+
+        # media_diaria_esperada_ame = round(agrupa_ame_conta['dhInicio'].agg('average'))
+        # #interpolar os resultados presentes para deduzir os pontos de mamada ausentes. https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.interpolate.html
+        # dIntervalo = date.fromisoformat(dataFim) - date.fromisoformat(dataInicio)
+        # hIntervalo = 24 / media_diaria_esperada_ame
+        # matriz_entradas = numpy.zeros((media_diaria_esperada_ame, dIntervalo.days))
+        # #quais os dias estão fora da média, para baixo?
+
+        # #listar as deduções em tabela
+
+        #gráfico de barras -- consumo diário de LM diário estimado
+        agrupa_ame_soma = agrupa_ame.agg({'volume': 'sum'})
+        mpl.figure()
+        agrupa_ame_soma.plot(y='volume', kind='bar')
+        mpl.savefig(nome_arquivo_exportacao('leiteMaterno', 'png'))
+        mpl.draw()
+
+        estimativa_diaria['hora'] = pandas.Series(estimativa_diaria['dhInicio'].dt.time, dtype='string')
+        estimativa_diaria['hora'] = estimativa_diaria['hora'].str.replace(':','', regex=False)
+        estimativa_diaria['hora'] = estimativa_diaria['hora'].str.slice(stop=4)
+        estimativa_diaria['hora'] = pandas.Series(estimativa_diaria['hora'], dtype='int')
+        mpl.figure()
+        estimativa_diaria.plot.scatter(x='hora', y='volume', use_index=False)
+        mpl.savefig(nome_arquivo_exportacao('scatterLeiteMaterno', 'png'))
+        mpl.draw()
     else:
-        #AMEC estimator?
-        pass
+        leiteMaterno_mesclado = pandas.concat([resLeite, dfP], ignore_index=True)
 
-    leiteMaterno_mesclado = pandas.concat([resLeite, dfP], ignore_index=True)
-    #agrupar leiteMaterno_mesclado por data
-    horarios_leitem = leiteMaterno_mesclado['dhInicio']
-    leiteMaterno_mesclado['dh'].fillna(horarios_leitem, inplace=True)
-    leiteMaterno_mesclado['data'] = leiteMaterno_mesclado['dh'].dt.date
-    leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['dh'].dt.time
+        #se houver registros de LM em mamadeira, organziar as colunas de data e hora, além de obter a quantidade de leite ingerido por mamadeira
+        if resLeite.empty == False:
+            horarios_leitem = leiteMaterno_mesclado['dhInicio']
+            volume_mamadeira_somado = pandas.Series()
+            leiteMaterno_mesclado['dh'].fillna(horarios_leitem, inplace=True)
+            leiteMaterno_mesclado['data'] = leiteMaterno_mesclado['dh'].dt.date
+            leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['dh'].dt.time
+            resLeite['data'] = resLeite['dh'].dt.date
+            volume_mamadeira_somado = resLeite.groupby('data')['volume'].apply(numpy.sum)
 
-    #para cada DF do agrupamento, montar uma list contendo a quantidade de mamadas encontradas
-    qtd_mamadas = leiteMaterno_mesclado.groupby('data').count()
+        #para cada DF do agrupamento, montar uma list contendo a quantidade de mamadas encontradas
+        qtd_mamadas = leiteMaterno_mesclado.groupby('data').count()
 
-    resLeite['data'] = resLeite['dh'].dt.date
-    volume_mamadeira_somado = resLeite.groupby('data')['volume'].apply(numpy.sum)
-    estimativa_diaria = []
-    for index, valor in qtd_mamadas['dhInicio'].iteritems():
-        estimativa_diaria.append(estimador_amamentacao_nMamadas(valor, idadec, index))
+        #estimando o volume de cada mamada no seio materno
+        estimativa_diaria = []
+        for index, valor in qtd_mamadas['dhInicio'].iteritems():
+            estimativa_diaria.append(estimador_amamentacao_nMamadas(valor, idadec, index))
+            #AMEC estimator?
 
-    estimativa_diaria = pandas.Series(estimativa_diaria, qtd_mamadas.index)
+        estimativa_diaria = pandas.Series(estimativa_diaria, qtd_mamadas.index)
 
-    if amex and nMamadeira > 0:
-        resultados_estimadores = pandas.DataFrame([estima_volume_amex, estima_volume_nMamadas])
-        resultados_estimadores['data'] = resultados_estimadores['dh'].dt.date
-        resultados_estimadores['hora'] = resultados_estimadores['dh'].dt.time
-        resultados_estimadores.drop('dh',1)
-        resultados_estimadores.groupby('data').agg({'volume': numpy.average})
+        if amex and nMamadeira > 0:
+            #se, apesar de utilizar mamadeira, somente leite materno é oferecido
+            estimativa_diaria['data'] = estimativa_diaria['dh'].dt.date
+            estimativa_diaria['hora'] = estimativa_diaria['dh'].dt.time
+            estimativa_diaria.drop('dh',1)
+            estimativa_diaria.groupby('data').agg({'volume': numpy.average})
+
+            mpl.figure()
+            estimativa_diaria.plot(x='data', y='volume', kind='bar', use_index=False)
+            mpl.savefig(nome_arquivo_exportacao('leiteMaterno', 'png'))
+            mpl.draw()
+        else:
+            if amex==False:
+                mpl.figure()
+                estimativa_diaria.plot(kind='bar')
+                mpl.savefig(nome_arquivo_exportacao('leiteMaterno', 'png'))
+                mpl.draw()
+
+        media_por_mamada_seio = ((estimativa_diaria-volume_mamadeira_somado) / qtd_mamadas['dh']).repeat(qtd_mamadas['dhInicio'].array) #comparar em gráfico de barras empilhadas os dois tipos de leite, invés da barra somatório
+        media_por_mamada_seio.rename('idx', inplace=True)
+        media_por_mamada_seio.index = leiteMaterno_mesclado[leiteMaterno_mesclado['volume'].isna()].index
+        #plotar: volumes (barras) e scatter (horarios); as mamadeiras em legenda especial no caso do scatter
+        leiteMaterno_mesclado['volume'].fillna(media_por_mamada_seio, inplace=True)
+        leiteMaterno_mesclado['hora'] = pandas.Series(leiteMaterno_mesclado['dh'].dt.time, dtype='string')
+        leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['hora'].str.replace(':','', regex=False)
+        leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['hora'].str.slice(stop=4)
+        leiteMaterno_mesclado['hora'] = pandas.Series(leiteMaterno_mesclado['hora'], dtype='int')
 
         mpl.figure()
-        resultados_estimadores.plot(x='data', y='volume', kind='bar', use_index=False)
-        mpl.savefig(nome_arquivo_exportacao('leiteMaterno', 'png'))
-    else:
-        if amex==False:
-            mpl.figure()
-            estimativa_diaria.plot(kind='bar')
-            mpl.savefig(nome_arquivo_exportacao('somaLeiteMaterno', 'png'))
-        else:
-            pass
-    media_por_mamada_seio = ((estimativa_diaria-volume_mamadeira_somado) / qtd_mamadas['dh']).repeat(qtd_mamadas['dhInicio'].array) #comparar em gráfico de barras empilhadas os dois tipos de leite, invés da barra somatório
-    media_por_mamada_seio.rename('idx', inplace=True)
-    media_por_mamada_seio.index = leiteMaterno_mesclado[leiteMaterno_mesclado['volume'].isna()].index
-    leiteMaterno_mesclado['volume'].fillna(media_por_mamada_seio, inplace=True)
-    leiteMaterno_mesclado['hora'] = pandas.Series(leiteMaterno_mesclado['dh'].dt.time, dtype='string')
-    leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['hora'].str.replace(':','', regex=False)
-    leiteMaterno_mesclado['hora'] = leiteMaterno_mesclado['hora'].str.slice(stop=4)
-    leiteMaterno_mesclado['hora'] = pandas.Series(leiteMaterno_mesclado['hora'], dtype='int')
-
-    mpl.figure()
-    leiteMaterno_mesclado.plot.scatter(x='hora', y='volume', use_index=False)
-    mpl.savefig(nome_arquivo_exportacao('scatterLeiteMaterno', 'png'))
+        leiteMaterno_mesclado.plot.scatter(x='hora', y='volume', use_index=False)
+        mpl.savefig(nome_arquivo_exportacao('scatterLeiteMaterno', 'png'))
+        mpl.draw()
 
 if nMamadeira > 0:
     #retirar resultados contendo leite materno
